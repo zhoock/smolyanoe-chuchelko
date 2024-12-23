f4e1bb1ff15c27f88a813b2c73c9f28e6a893a38
Yaroslav Zhoock <zhoock@zhoock.ru>
2024-12-23 21:25:12 +0500

    fixed bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,90 @@
-const path = require("path"); // модуль nodejs для корректной обработки путей
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // Плагин для генерации HTML с правильными путями к скриптам
-const FileManagerPlugin = require("filemanager-webpack-plugin"); // автоматическая очистка каталогов
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // извлекаем CSS из файлов .js при сборке
+const path = require('path'); // модуль nodejs для корректной обработки путей
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Плагин для генерации HTML с правильными путями к скриптам
+const FileManagerPlugin = require('filemanager-webpack-plugin'); // автоматическая очистка каталогов
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // извлекаем CSS из файлов .js при сборке
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    bundle: "./src/index.tsx", // Основной файл для React с TypeScript
+    bundle: './src/index.tsx', // Основной файл для React с TypeScript
     // modules: "./src/script.js", // Дополнительный скрипт на JavaScript
   },
 
   output: {
-    filename: "index[contenthash].js", // // Имя итогового бандла, используем contenthash для динамических хешей
-    path: path.join(__dirname, "dist"), // Папка для собранных файлов
-    assetModuleFilename: path.join("images", "[name].[contenthash][ext]"),
+    filename: 'index[contenthash].js', // // Имя итогового бандла, используем contenthash для динамических хешей
+    path: path.join(__dirname, 'dist'), // Папка для собранных файлов
+    assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
     clean: true, // Очищать старые файлы при сборке
   },
 
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"], // Поддерживаемые расширения
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'], // Поддерживаемые расширения
   },
 
   module: {
     rules: [
       {
         test: /\.tsx?$/, // Обработка TypeScript файлов
-        use: "ts-loader", // Используем ts-loader для компиляции TypeScript
+        use: 'ts-loader', // Используем ts-loader для компиляции TypeScript
         exclude: /node_modules/,
       },
       {
         test: /\.(js|jsx)$/, // Обработка JS файлов через Babel
-        use: "babel-loader", // Используем babel-loader для JS
+        use: 'babel-loader', // Используем babel-loader для JS
       },
       {
         test: /\.pug$/,
-        loader: "pug-loader",
+        loader: 'pug-loader',
         options: {
-          attrs: ["img:src", "source:srcset"],
+          attrs: ['img:src', 'source:srcset'],
         },
       },
       {
         test: /\.(scss|sass|css)$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
         ],
       },
       {
         test: /\.(png|jpe?g|webp|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.svg$/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: path.join("icons", "[name].[contenthash][ext]"),
+          filename: path.join('icons', '[name].[contenthash][ext]'),
         },
       },
     ],
   },
 
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/_redirects'), // Путь к файлу _redirects
+          to: path.resolve(__dirname, './dist/_redirects'), // Папка в которую нужно скопировать
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
-      template: "./src/index.html", // Указываем исходный HTML-шаблон
-      inject: "body", // Скрипты будут вставляться перед закрывающим тегом </body>
-      filename: "index.html", // Имя итогового HTML файла
+      template: './src/index.html', // Указываем исходный HTML-шаблон
+      inject: 'body', // Скрипты будут вставляться перед закрывающим тегом </body>
+      filename: 'index.html', // Имя итогового HTML файла
     }),
     new FileManagerPlugin({
       events: {
         onStart: {
-          delete: ["dist"],
+          delete: ['dist'],
         },
       },
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash:6].css",
+      filename: '[name].[contenthash:6].css',
     }),
   ],
   performance: {
@@ -83,7 +92,7 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true, // необходимо при испольтзовании React Router для маршрутизации
-    watchFiles: path.join(__dirname, "src"), // указывает на каталог src, за которыми будет вестись наблюдение
+    watchFiles: path.join(__dirname, 'src'), // указывает на каталог src, за которыми будет вестись наблюдение
     port: 8080, // указывает порт на котором будет работать веб-сервер
     hot: true, // включает горячую перезагрузку
   },
