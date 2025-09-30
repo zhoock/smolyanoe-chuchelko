1d0092b433c97d8596b76da5837fc8deef0e79d9
Yaroslav Zhoock <zhoock@zhoock.ru>
2025-09-30 12:13:48 +0500

    fix: орпор

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import { LangProvider } from './contexts/lang';
-import { HelmetProvider } from 'react-helmet-async';
 import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LangProvider>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
+      <App />
     </LangProvider>
   </React.StrictMode>
 );
---
+++ test.ts
@@ -0,0 +1,3 @@
+const add = (a: number, b: number) => a + b;
+
+console.log(add(2, 3)); // 5
