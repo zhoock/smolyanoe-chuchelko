46373577bb17b9a1800d62242bae235b6b74a436
Yaroslav Zhoock <zhoock@zhoock.ru>
2025-11-28 17:20:17 +0300

    fix: исправлена структура скелетона - элементы сгруппированы правильно
    - Обложка, номер 23 и Share в одном блоке (слева)
    - Кнопка Play и 3 трека в другом блоке (справа)
    - Все 8 элементов присутствуют с правильной структурой

--- src/shared/ui/skeleton/AlbumPageSkeleton.tsx
+++ src/shared/ui/skeleton/AlbumPageSkeleton.tsx
@@ -13,53 +13,32 @@ export function AlbumPageSkeleton() {
           </ul>
         </nav>
 
-        {/* 2. Album cover */}
+        {/* 2. Album cover, 3. Number "23", 8. Share icon */}
         <div className="item">
           <div className="skeleton skeleton--album-cover" />
-        </div>
-
-        {/* 3. Number "23" */}
-        <div className="item">
           <div className="skeleton skeleton--album-number" />
+          <div className="skeleton skeleton--share-button" />
         </div>
 
-        {/* 4. Play button "Воспроизвести" */}
+        {/* 4. Play button "Воспроизвести", 5. Track 1, 6. Track 2, 7. Track 3 */}
         <div className="item">
           <div className="wrapper-album-play">
             <div className="skeleton skeleton--play-button" />
           </div>
-        </div>
-
-        {/* 5. Track 1 */}
-        <div className="item">
           <div className="tracks">
-            <div className="tracks__btn">
-              <div className="skeleton skeleton--text skeleton--track-title" />
-            </div>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <div
+                key={index}
+                className="tracks__btn"
+                style={{ '--skeleton-index': index } as React.CSSProperties}
+              >
+                <div className="tracks__symbol">
+                  <div className="skeleton skeleton--track-number" />
+                </div>
+                <div className="skeleton skeleton--text skeleton--track-title" />
+              </div>
+            ))}
           </div>
-        </div>
-
-        {/* 6. Track 2 */}
-        <div className="item">
-          <div className="tracks">
-            <div className="tracks__btn">
-              <div className="skeleton skeleton--text skeleton--track-title" />
-            </div>
-          </div>
-        </div>
-
-        {/* 7. Track 3 */}
-        <div className="item">
-          <div className="tracks">
-            <div className="tracks__btn">
-              <div className="skeleton skeleton--text skeleton--track-title" />
-            </div>
-          </div>
-        </div>
-
-        {/* 8. Share icon */}
-        <div className="item">
-          <div className="skeleton skeleton--share-button" />
         </div>
       </div>
     </section>
