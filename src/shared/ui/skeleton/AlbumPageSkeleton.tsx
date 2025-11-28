fce14cfafa3cc13780dbe817a8348bd73e1fdd00
Yaroslav Zhoock <zhoock@zhoock.ru>
2025-11-28 17:17:04 +0300

    refactor: скелетон содержит только 8 элементов, отмеченных красным
    - Только breadcrumb, обложка, число 23, кнопка Play, 3 трека и Share
    - Каждый элемент в отдельном блоке для точного соответствия скриншоту

--- src/shared/ui/skeleton/AlbumPageSkeleton.tsx
+++ src/shared/ui/skeleton/AlbumPageSkeleton.tsx
@@ -4,7 +4,7 @@ export function AlbumPageSkeleton() {
   return (
     <section className="album main-background" aria-label="Блок c альбомом">
       <div className="wrapper album__wrapper">
-        {/* Breadcrumb skeleton */}
+        {/* 1. Breadcrumb "Главная" */}
         <nav className="breadcrumb item-type-a" aria-label="Breadcrumb">
           <ul>
             <li>
@@ -13,32 +13,53 @@ export function AlbumPageSkeleton() {
           </ul>
         </nav>
 
-        {/* Album cover skeleton */}
+        {/* 2. Album cover */}
         <div className="item">
           <div className="skeleton skeleton--album-cover" />
-          <div className="skeleton skeleton--album-number" />
-          <div className="skeleton skeleton--share-button" />
         </div>
 
-        {/* Album tracks skeleton */}
+        {/* 3. Number "23" */}
+        <div className="item">
+          <div className="skeleton skeleton--album-number" />
+        </div>
+
+        {/* 4. Play button "Воспроизвести" */}
         <div className="item">
           <div className="wrapper-album-play">
             <div className="skeleton skeleton--play-button" />
           </div>
+        </div>
+
+        {/* 5. Track 1 */}
+        <div className="item">
           <div className="tracks">
-            {Array.from({ length: 3 }).map((_, index) => (
-              <div
-                key={index}
-                className="tracks__btn"
-                style={{ '--skeleton-index': index } as React.CSSProperties}
-              >
-                <div className="tracks__symbol">
-                  <div className="skeleton skeleton--track-number" />
-                </div>
-                <div className="skeleton skeleton--text skeleton--track-title" />
-              </div>
-            ))}
+            <div className="tracks__btn">
+              <div className="skeleton skeleton--text skeleton--track-title" />
+            </div>
           </div>
+        </div>
+
+        {/* 6. Track 2 */}
+        <div className="item">
+          <div className="tracks">
+            <div className="tracks__btn">
+              <div className="skeleton skeleton--text skeleton--track-title" />
+            </div>
+          </div>
+        </div>
+
+        {/* 7. Track 3 */}
+        <div className="item">
+          <div className="tracks">
+            <div className="tracks__btn">
+              <div className="skeleton skeleton--text skeleton--track-title" />
+            </div>
+          </div>
+        </div>
+
+        {/* 8. Share icon */}
+        <div className="item">
+          <div className="skeleton skeleton--share-button" />
         </div>
       </div>
     </section>
